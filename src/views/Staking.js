f01c77eb65fc83cc52b86a03f4539b89ed945406
prettypet2137 <prettypet2137@gmail.com>
2023-03-20 14:57:54 +0900

    staking table bug fix

--- src/views/Staking.js
+++ src/views/Staking.js
@@ -122,8 +122,8 @@ export default function Staking() {
                         <td>{r.token}</td>
                         <td>{formatterFloat(r.amount)}</td>
                         <td>{formatterFloat(r.share)}%</td>
-                        <td>{`${r.apr_hex}%`} $HEX<br/>{formatterFloat(r.apr_hexit)} $HEXIT</td>
-                        <td>{`${r.earned_hex}%`} $HEX<br/>{formatterFloat(r.earned_hexit)} $HEXIT</td>
+                        <td>{`${r.apr_hex}%`} $HEX<br/>{formatterFloat(r.apr_hexit)}% $HEXIT</td>
+                        <td>{`${r.earned_hex}`} $HEX<br/>{formatterFloat(r.earned_hexit)} $HEXIT</td>
                         <td>{formatterFloat(r.days)}</td>
                         <td>$ {formatterFloat(r.liquidity)}</td>
                         <td>{r.multiplier}x</td>
