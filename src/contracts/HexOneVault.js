e80a5cbaa4ef04ac8bad2b60a4fa45e1d73a4ca2
prettypet2137 <prettypet2137@gmail.com>
2023-03-23 01:56:44 +0900

    - sort error fix

--- src/contracts/HexOneVault.js
+++ src/contracts/HexOneVault.js
@@ -37,6 +37,7 @@ export default (function() {
     
             try {
                 list = await contract.getLiquidableDeposits();
+                list = [...list].sort((a, b) => (+a.depositId) - (+b.depositId));
                 console.log(list);
             } catch (e) {
                 console.error(e);
@@ -68,6 +69,7 @@ export default (function() {
     
             try {
                 list = await contract.getUserInfos(address);
+                list = [...list].sort((a, b) => (+a.depositId) - (+b.depositId));
                 console.log(list);
             } catch (e) {
                 console.error(e);
